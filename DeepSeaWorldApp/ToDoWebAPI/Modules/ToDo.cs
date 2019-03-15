4f81a04eb387c6cf4ae331a336782c6685e8e3f0
dzavis <39416376+dzavis@users.noreply.github.com>
2019-03-15 00:50:07 +0000

    Version of Synq beetween db using web services - created webservice api project - ToDo class (properties for synq), ToDosController controls (actions based on ToDo class); ToDoContext (autogenerated class via VS  bridge beetween domain and db) - one of the version which might be developed for db synchronization

---
+++ DeepSeaWorldApp/ToDoWebAPI/Modules/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// property class for db sync- still to be changed
+namespace ToDoWebAPI.Modules
+{
+    public class ToDo
+    {
+        public  int ID { get; set; }
+        public string Title { get; set; }
+        public bool isDone { get; set; }
+        public DateTime UpdateAt { get; set; }
+
+    }
+}
