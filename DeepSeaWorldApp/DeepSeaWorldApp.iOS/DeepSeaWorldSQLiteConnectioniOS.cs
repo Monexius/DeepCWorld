caa6c1d4fc81f323347857558711ba1ebdeadb52
dzavis <39416376+dzavis@users.noreply.github.com>
2019-04-14 20:45:18 +0100

    Code cleanup

--- DeepSeaWorldApp/DeepSeaWorldApp.iOS/DeepSeaWorldSQLiteConnectioniOS.cs
+++ DeepSeaWorldApp/DeepSeaWorldApp.iOS/DeepSeaWorldSQLiteConnectioniOS.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace DeepSeaWorldApp.iOS
 {
-    class DeepSeaWorldSQLiteConnectioniOS : DeepSeaWorldSQLiteInterface
+    class DeepSeaWorldSQLiteConnectioniOS 
     {
 
         MySqlDBCon mySql = new MySqlDBCon();
