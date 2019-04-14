3b090bd40869573415501a647d0868e7bced26e8
dzavis <39416376+dzavis@users.noreply.github.com>
2019-04-14 21:18:51 +0100

    iOS code cleanup

--- DeepSeaWorldApp/DeepSeaWorldApp.iOS/Main.cs
+++ DeepSeaWorldApp/DeepSeaWorldApp.iOS/Main.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using DeepSeaWorldApp;
+﻿using DeepSeaWorldApp;
 using DeepSeaWorldApp.iOS;
-using Foundation;
 using UIKit;
 using static DeepSeaWorldApp.DBClasses.DBs;
 
